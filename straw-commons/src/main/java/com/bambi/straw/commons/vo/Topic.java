6870f912447399e23d61d0852c0864117f99bbde
RichardReindeer <44000760+RichardReindeer@users.noreply.github.com>
2021-10-06 21:25:38 +0800

    2021/10/06 comment 相关修改

--- straw-commons/src/main/java/com/bambi/straw/commons/vo/Topic.java
+++ straw-commons/src/main/java/com/bambi/straw/commons/vo/Topic.java
@@ -1,10 +1,22 @@
 package com.bambi.straw.commons.vo;
 
 /**
- * 因为就是个常量，就不用注入了
+ * 描述：
+ *  Kafka的Topic类
+ * <pre>
+ * HISTORY
+ * ****************************************************************************
+ *  ID     DATE          PERSON          REASON
+ *  1      2021/10/6 21:05    Bambi        Create
+ * ****************************************************************************
+ * </pre>
+ *
+ * @author Bambi
+ * @since 1.0
  */
 public class Topic {
-    //统一faq模块和search模块中发送的话题名称
+
+    //指定topic
     public static final String QUESTIONS = "search.questions";
 
 }
--- straw-faq/src/main/java/com/bambi/straw/faq/controller/CommentController.java
+++ straw-faq/src/main/java/com/bambi/straw/faq/controller/CommentController.java
@@ -1,11 +1,11 @@
 package com.bambi.straw.faq.controller;
 
-
 import com.bambi.straw.commons.model.Comment;
 import com.bambi.straw.commons.vo.R;
 import com.bambi.straw.faq.service.ICommentService;
 import com.bambi.straw.faq.vo.CommentVo;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,64 +14,102 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * <p>
- *  前端控制器
- * </p>
+ * 描述：
+ * 回复类请求前端控制器类
+ * <pre>
+ * HISTORY
+ * ****************************************************************************
+ *  ID     DATE          PERSON          REASON
+ *  1      2021/10/6 21:08    Bambi        Create
+ * ****************************************************************************
+ * </pre>
  *
- * @author MR.Bambi
- * @since 2021-04-14
+ * @author Bambi
+ * @since 1.0
  */
 @RestController
 @RequestMapping("/v1/comments")
-@Slf4j
 public class CommentController {
 
-    @Autowired//spring 给的
+    private static Logger logger = LoggerFactory.getLogger(CommentController.class);
+
+    @Autowired
     private ICommentService commentService;
-    @PostMapping
+
+    /**
+     * 对回复进行保存
+     *
+     * @param commentVo   接收到的回复信息
+     * @param result      验证
+     * @param userDetails 用户信息
+     * @return 标准R类
+     */
+    @RequestMapping
     public R<Comment> postComment(
             @Validated CommentVo commentVo, BindingResult result,
             @AuthenticationPrincipal UserDetails userDetails
-            ){
-        log.debug("收到的评论信息为:{}",commentVo);
-        if(result.hasErrors()){
-            //获得字符串类型的错误信息
-            String message = result.getFieldError().getDefaultMessage();
-            //返回不可用实体错误
-            return R.unproecsableEntity(message);
+    ) {
+        logger.info("postComment is starting ! ! !");
+        if (result.hasErrors()) {
+            logger.error("has some problems in postComment , result has Error");
+            return R.unproecsableEntity(result.getFieldError().getDefaultMessage());
         }
+        logger.info("postComment is finish");
+        return R.created(commentService.saveComment(commentVo, userDetails.getUsername()));
+    }
 
-        //调用业务逻辑层代码
-        Comment comment = commentService.saveComment(commentVo, userDetails.getUsername());
-        //返回
-        return R.created(comment);
-    };
-
-
-    //删除评论的控制层方法
-    @GetMapping("/{id}/delete")
+    /**
+     * 删除回复
+     *
+     * @param id          回复id
+     * @param userDetails 用户详情
+     * @return
+     */
+    @RequestMapping("/{id}/delete")
     public R removeComment(
             @PathVariable Integer id,
             @AuthenticationPrincipal UserDetails userDetails
-    ){
-        if(id==null) R.notFound("Id不存在");
-        //调用业务逻辑层
-        boolean isDelete = commentService.removeComment(id,userDetails.getUsername());
-        if(isDelete){
-            return R.gone("删除完成");
-        }else {
-            return R.notFound("删除失败，请检查");
+    ) {
+        logger.info("removeComment is Starting ! ! !");
+        if (id == null) {
+            logger.error("id is null");
+            return R.notFound("id 不存在");
+        }
+        boolean isDelete = commentService.removeComment(id, userDetails.getUsername());
+        if (isDelete) {
+            logger.info("删除成功");
+            return R.gone("删除成功");
+        } else {
+            logger.error("删除失败，check the pathVal");
+            return R.notFound("删除失败");
         }
     }
 
-    @PostMapping("/{id}/update")
+
+    /**
+     * 对回复进行修改
+     * @param id
+     * @param content
+     * @param userDetails
+     * @return
+     */
+    @RequestMapping("/{id}/update")
     public R<Comment> updateComment(
             @PathVariable Integer id,
             String content,
             @AuthenticationPrincipal UserDetails userDetails
-    ){
-        Comment comment = commentService.updateComment(id,content, userDetails.getUsername());
-        return R.ok(comment);
+    ) {
+        logger.info("updateComment is starting ! !");
+        if (id == null) {
+            logger.error("id is not found");
+            return R.notFound("id is not found");
+        }
+        if (userDetails == null) {
+            logger.error("userDetails is null");
+            return R.notFound("userDetails is null");
+        }
+
+        return R.ok(commentService.updateComment(id, content, userDetails.getUsername()));
     }
 
 }
